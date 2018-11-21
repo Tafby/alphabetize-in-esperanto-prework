6cff43aa69b48faf95cae248bfe0805ef12bae4c
Tiffany Carter <tafby88@gmail.com>
2018-11-21 01:31:12 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@
 
 def alphabetize(arr)
   alaphbet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  array.sort_by do |word|
+  arr.sort_by do |word|
   word.split('').map do |letter|
     alaphbet.index(letter)
     end
